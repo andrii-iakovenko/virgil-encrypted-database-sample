3e5f73b14c772cc025fad1e21f32bb28e89d4898
Andrii Iakovenko <ayakovenko@exadel.com>
2016-06-28 11:04:12 +0300

    Create sample for storing encrypted date in SQL Lite

---
+++ app/src/main/java/com/virgilsecurity/encrypteddatabase/MessageEntry.java
@@ -0,0 +1,11 @@
+package com.virgilsecurity.encrypteddatabase;
+
+/**
+ * Created by Andrii Iakovenko on 27.06.16.
+ */
+public class MessageEntry {
+
+    public static final String TABLE_NAME = "messages";
+    public static final String _ID = "message_id";
+    public static final String COLUMN_NAME_MESSAGE = "message";
+}
